1a5e815a7f222ffee5dc04666b0f481a61d4ee22
Rian Alves <alvesrian49.com@gmail.com>
2024-02-07 22:21:26 -0300

    --COD BUGADO--
    Fazendo um teste pra entender melhor o Github.

--- login/src/main.js
+++ login/src/main.js
@@ -9,3 +9,12 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+const registro = document.getElementById('registro');
+const container = document.getElementById('container');
+const login = document.getElementById('login');
+
+login.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
